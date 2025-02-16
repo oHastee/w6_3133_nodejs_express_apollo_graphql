209f8350b89598b2383a1dda7689d71666be4fec
oHastee <oscarpiediaz@gmail.com>
2025-02-16 16:37:52 -0500

    Created resolvers.js

--- resolvers.js
+++ resolvers.js
@@ -1,1 +1,39 @@
-const Movie = require('./models/Movie');
+let movies = [
+    {
+        id: "1",
+        name: "Inception",
+        director_name: "Christopher Nolan",
+        production_house: "Warner Bros",
+        release_date: "2010",
+        rating: 8.8
+    },
+];
+
+const resolvers = {
+    Query: {
+        movies: () => movies,
+        movie: (parent, args) => movies.find(movie => movie.id === args.id)
+    },
+    Mutation: {
+        addMovie: (parent, args) => {
+            const newMovie = { id: String(movies.length + 1), ...args };
+            movies.push(newMovie);
+            return newMovie;
+        },
+        updateMovie: (parent, args) => {
+            const index = movies.findIndex(movie => movie.id === args.id);
+            if (index === -1) return null;
+            movies[index] = { ...movies[index], ...args };
+            return movies[index];
+        },
+        deleteMovie: (parent, args) => {
+            const index = movies.findIndex(movie => movie.id === args.id);
+            if (index === -1) return null;
+            const deletedMovie = movies[index];
+            movies = movies.filter(movie => movie.id !== args.id);
+            return deletedMovie;
+        }
+    }
+};
+
+module.exports = resolvers;
